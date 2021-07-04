956e7fb7c6dcc17a04870daf30e1d40713129b58
Dmytro Turskyi <dmitriy.turskiy@gmail.com>
2021-07-04 16:36:04 +0300

    add saving and deleting files

--- app/src/main/java/io/github/turskyi/sdcard/MainActivity.kt
+++ app/src/main/java/io/github/turskyi/sdcard/MainActivity.kt
@@ -10,15 +10,16 @@ import android.os.Bundle
 import android.os.Environment
 import android.view.View
 import android.view.View.VISIBLE
+import android.view.ViewGroup
 import android.widget.*
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.AdapterView.OnItemLongClickListener
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
-import java.io.File
-import java.io.FileNotFoundException
-import java.io.FileOutputStream
-import java.io.IOException
+import java.io.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -40,11 +41,18 @@ class MainActivity : AppCompatActivity() {
 
     private fun initListeners() {
         val button: Button = findViewById(R.id.button)
+        val buttonCache: Button = findViewById(R.id.btnCache)
         val saveFileButton = findViewById<Button>(R.id.button_save)
         val fileNameEditText = findViewById<TextInputEditText>(R.id.edit_filename)
         val contentEditText = findViewById<TextInputEditText>(R.id.edit_content)
         val getFileListButton: Button = findViewById(R.id.button_get_file_list)
         val savedFilesListView: ListView = findViewById(R.id.list_files)
+
+        buttonCache.setOnClickListener {
+            // if we need to save files we can save them in cache
+            val file = cacheDir
+            Toast.makeText(this, file.toString(), Toast.LENGTH_LONG).show()
+        }
         button.setOnClickListener {
             val action: String = Intent.ACTION_OPEN_DOCUMENT
             val intent = Intent(action)
@@ -91,6 +99,72 @@ class MainActivity : AppCompatActivity() {
             // show list of files
             savedFilesListView.adapter = adapter
         }
+
+        savedFilesListView.onItemClickListener =
+            OnItemClickListener { parent, view, position, id ->
+                val clickedFile = parent
+                    .getItemAtPosition(position) as String
+                openFileDialog(clickedFile)
+            }
+
+        savedFilesListView.onItemLongClickListener =
+            OnItemLongClickListener { parent, view, position, id ->
+                val longClickedFile = parent
+                    .getItemAtPosition(position) as String
+                // delete file
+                deleteFile(longClickedFile)
+                Toast.makeText(
+                    applicationContext,
+                    "$longClickedFile удалён", Toast.LENGTH_LONG
+                )
+                    .show()
+                showSavedFiles()
+                true
+            }
+
+    }
+
+    private fun showSavedFiles() {
+        val savedFilesListView: ListView = findViewById(R.id.list_files)
+        val savedFilesArray = fileList()
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1, savedFilesArray
+        )
+        savedFilesListView.adapter = adapter
+    }
+
+    private fun openFileDialog(file: String) {
+        // read file
+        val fis: FileInputStream
+        val content = StringBuilder()
+        try {
+            fis = openFileInput(file) // открываем файл для чтения
+            val input = ByteArray(fis.available())
+            while (fis.read(input) != -1) {
+                content.append(String(input))
+            }
+            fis.close()
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        // create dialogue window
+        val fileDialog: AlertDialog.Builder = AlertDialog.Builder(
+            this
+        )
+        fileDialog.setTitle(file)
+        val contentTextView = TextView(this)
+        contentTextView.text = content.toString()
+        val textViewLayoutParams = ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        contentTextView.layoutParams = textViewLayoutParams
+        fileDialog.setView(contentTextView)
+        fileDialog.setPositiveButton("OK", null)
+        fileDialog.show()
     }
 
     private fun initResultLauncher() {
@@ -183,6 +257,7 @@ class MainActivity : AppCompatActivity() {
         val mInfoTextView = findViewById<TextView>(R.id.tvExternalCacheDirs)
         // show paths of cache directories: internal and external
         mInfoTextView.text = info
+
     }
 
     private fun showPathsToFiles() {
